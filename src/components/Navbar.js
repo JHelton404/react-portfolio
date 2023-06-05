c6dc97b472335ce29279d2c72917f553f064fdb6
Jhelt <jonhelton405@gmail.com>
2023-06-05 18:54:02 -0400

    styled the navbar, Navbar is turing complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,19 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/Navbar.css';
 
 function Navbar() {
   return (
-    <div className='navbar' >
+    <div className='navbar'>
+      <h1 className='myName'>Jon Helton</h1>
       <div className='links'>
         <Link to="/">About Me</Link>
         <Link to="/portfolio">Portfolio</Link>
         <Link to="/contact">Contact</Link>
         <Link to="/resume">Resume</Link>
       </div>
-
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
